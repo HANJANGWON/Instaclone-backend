1df6f842dc7669949fcfff541a11792bbbf4435e
한장원 <hanjang-won@hanjang-won-ui-MacBookPro.local>
2021-07-04 20:29:07 +0900

    user module

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+import { getUser, protectResolver } from "./users/users.utils";
+
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      protectResolver,
+    };
+  },
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log(`✌️ Server is running on http://localhost:${PORT}/`));
